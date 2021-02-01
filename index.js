ed43517cdd9f8700b243eb214eee22fba8cf7da2
Attila Ujj <attila.ujj@botium.at>
2021-02-01 09:15:54 +0100

    UI for editing caps

--- index.js
+++ index.js
@@ -4,6 +4,36 @@ module.exports = {
   PluginVersion: 1,
   PluginClass: PluginClass,
   PluginDesc: {
-    name: 'Botium Connector for SMS Messaging (with Twilio)'
+    name: 'Botium Connector for SMS Messaging (with Twilio)',
+    capabilities: [
+      {
+        name: 'TWILIO_SMS_ACCOUNT_SID',
+        label: 'Twilio Account SID',
+        description: 'Account SID from Twilio account',
+        type: 'string',
+        required: true
+      },
+      {
+        name: 'TWILIO_SMS_AUTH_TOKEN',
+        label: 'Twilio Auth Token',
+        description: 'Auth Token from Twilio account',
+        type: 'secret',
+        required: true
+      },
+      {
+        name: 'TWILIO_SMS_FROM',
+        label: 'Caller Id',
+        description: 'Purchased or Verified phone number from Twilio account',
+        type: 'string',
+        required: true
+      },
+      {
+        name: 'TWILIO_SMS_TO',
+        label: 'SMS Phone Number',
+        description: 'Phone number to send message',
+        type: 'string',
+        required: true
+      }
+    ]
   }
 }
