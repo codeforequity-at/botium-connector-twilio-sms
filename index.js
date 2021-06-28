996ae130b93970fb9da5e4c89fd2dd39a2cccdbe
Florian Treml <florian.treml@botium.at>
2021-06-28 15:17:46 +0200

    updated logo

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const fs = require('fs')
+const path = require('path')
 const PluginClass = require('./src/connector')
+
+const logo = fs.readFileSync(path.join(__dirname, 'logo.png')).toString('base64')
 
 module.exports = {
   PluginVersion: 1,
   PluginClass: PluginClass,
   PluginDesc: {
     name: 'Botium Connector for SMS Messaging (with Twilio)',
+    avatar: logo,
     capabilities: [
       {
         name: 'TWILIO_SMS_ACCOUNT_SID',
